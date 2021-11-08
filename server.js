a6e5a8b63c5a83533d18b999c8a73d76df344ce7
David <birdandsthebees@gmail.com>
2021-11-08 13:30:37 +0000

    organize application function to myApp

--- server.js
+++ server.js
@@ -64,10 +64,6 @@ app.use("/public", express.static(__dirname + "/public"));
 
 //Listen on port set in environment variable or default to 3000
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
-});
-
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function () {
   bGround.log("Node is listening on port " + port + "...");
