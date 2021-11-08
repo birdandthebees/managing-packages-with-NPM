e4075a57be621aa5f307f558877298f0739606cb
David <birdandsthebees@gmail.com>
2021-11-08 13:48:58 +0000

    change to es6 function

--- myApp.js
+++ myApp.js
@@ -5,14 +5,14 @@ var bGround = require("fcc-express-bground");
 bGround.log("Hello World");
 console.log("Hello World ");
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", function (req, res) {
+app.get("/json", (req, res) => {
   res.json({ message: "Hello json" });
 });
 
